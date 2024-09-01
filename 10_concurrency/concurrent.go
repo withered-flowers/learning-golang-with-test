c2e96d9eb05f57b1dbc3366d654d31225cb43763
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-09-01 09:14:01 +0700

    feat: learning done - concurrency

--- 10_concurrency/concurrent.go
+++ 10_concurrency/concurrent.go
@@ -1,19 +1,42 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+// ? Now we need to define the channel result struct
+type result struct {
+	// ? string for the web address
+	string
+	// ? bool for the validation result
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	// ? Concurrency need to be combined with channels
+	// ? Channels are the sender and receiver (take it as "room" in socket.io)
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			// results[u] = wc(u)
+
+			// ? Now instead of assigning to wc directly
+			// ? We will send the value via channel
+			resultsChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	// ? Sleep is not needed anymore
+	// time.Sleep(2 * time.Second)
+
+	// ? Now we will need to add logic to receive the sended value via channel
+	for i := 0; i < len(urls); i++ {
+		// ? Now we will receive the channel value, which is result struct
+		r := <-resultsChannel
+
+		// ? We will assign the received value to results
+		results[r.string] = r.bool
+	}
 
 	return results
 }
